7ee634e37265e852ef76afdf2d04b4c828524b78
Ali Can Güneş <cgunes52@gmail.com>
2021-06-06 14:46:18 +0300

    calculator is finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState("");
+  const calcBtns = [];
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach(item => {
+    calcBtns.push(
+      <button onClick={e => {
+        setData(data + e.target.value)
+      }}
+        value={item}
+        key={item}>
+        {item}
+      </button>
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="wrapper">
+      <div className="show-input">
+        {data}
+      </div>
+      <div className="digits flex">
+        {calcBtns}
+      </div>
+      <div className="modifiers subgrid">
+        <button onClick={() => setData(data.substr(0, data.length - 1))}>Clear</button>
+        <button onClick={() => setData("")}>AC</button>
+      </div>
+      <div className="operations subgrid">
+        <button onClick={e => setData(data + e.target.value)} value="+">+</button>
+        <button onClick={e => setData(data + e.target.value)} value="-">-</button>
+        <button onClick={e => setData(data + e.target.value)} value="*">*</button>
+        <button onClick={e => setData(data + e.target.value)} value="/">/</button>
+        <button
+          onClick={(e) => {
+            try {
+              setData(
+                String(eval(data)).length > 3 &&
+                  String(eval(data)).includes(".")
+                  ? String(eval(data).toFixed(4))
+                  : String(eval(data))
+              );
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+          value="="
         >
-          Learn React
-        </a>
-      </header>
+          =
+        </button>
     </div>
+    </div >
   );
 }
 
